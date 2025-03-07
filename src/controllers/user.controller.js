6e06ffea5df70adb0936a4e14bbf234226be8e0d
Ankithg21 <ankithg2104@gmail.com>
2025-03-07 13:29:11 +0530

    Add Subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -224,4 +224,4 @@ const refreshAccessToken = asyncHandler(async (req,res)=>{
     }
 });
 
-export {registerUser, loginUser, logoutUser};
+export {registerUser, loginUser, logoutUser, refreshAccessToken};
